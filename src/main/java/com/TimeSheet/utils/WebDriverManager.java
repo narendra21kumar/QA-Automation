94022f96cf74b032a9794dc62652132ca4b23af1
narendra21kumar <gnarendra.java@gmail.com>
2024-12-19 12:03:38 +0530

    First Commit

--- src/main/java/com/TimeSheet/utils/WebDriverManager.java
+++ src/main/java/com/TimeSheet/utils/WebDriverManager.java
@@ -1,6 +1,6 @@
 package com.TimeSheet.utils;
 
-
+import com.epam.healenium.SelfHealingDriver;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
@@ -8,17 +8,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverManager {
-    private static WebDriver driver;
-
+   // private static WebDriver driver;
+    static private SelfHealingDriver driver;
+    
     public static WebDriver getDriver() {
         if (driver == null) {
+        	
+        	
             ChromeOptions options=new ChromeOptions();
             String chromeArguments = ConfigReader.getChromeArguments();
             if(chromeArguments !=null && !chromeArguments.isEmpty())
             {
             	options.addArguments(chromeArguments.split("\\s+"));
             }
-            driver = new ChromeDriver(options);
+           
+            WebDriver delegate = new ChromeDriver(options);
+            driver= SelfHealingDriver.create(delegate);
+            
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             CommonUtils.waitFor(2);
             driver.manage().deleteAllCookies();
